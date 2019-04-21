46294c24deebec0fe832390ed38a7d89a8b0f0fd
PiyushKhurana <khuranapiyush27@gmail.com>
2019-04-21 20:00:03 +0530

    added card component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import CommentDetails from './CommentDetails';
- 
+ import ApprovalCard from './ApprovalCard'
 //create a react component
 const App=function(){
     
     return (
     <div className="ui container comments">
+    <ApprovalCard>
     <CommentDetails author="Sam" timeAgo="Today at 6:35 AM"/>
+    </ApprovalCard>
+    <ApprovalCard>
     <CommentDetails author="Alex" timeAgo="Today at 4:10 PM"/>
+    </ApprovalCard>
+    <ApprovalCard>
     <CommentDetails author="Jane" timeAgo="Yesterday at 9:00 PM"/>
+    </ApprovalCard>
     </div>
     );
 };
