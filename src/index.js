1aad9462ae5755aa62b0a314143045b85808e93a
@ROCKMURPHY <61891464+christcame@users.noreply.github.com>
2025-08-15 03:26:08 +0000

    Fix all errors and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-//mport react and react dom lib
+//import react and react dom lib
 import React from 'react';
-import ReactDom from 'react-dom';
-import faker from 'faker';
+import { createRoot } from 'react-dom/client';
+import { faker } from '@faker-js/faker';
 
 import CommentDetails from './CommentDetails';
- import ApprovalCard from './ApprovalCard'
+import ApprovalCard from './ApprovalCard';
+
 //create a react component
-const App=function(){
+const App = function(){
     
     return (
-    <div className="ui container comments">
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Today at 6:35 AM"/>
-    </ApprovalCard>
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Today at 4:10 PM"/>
-    </ApprovalCard>
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Yesterday at 9:00 PM"/>
-    </ApprovalCard>
-    </div>
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetails author={faker.person.fullName()} timeAgo="Today at 6:35 AM"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetails author={faker.person.fullName()} timeAgo="Today at 4:10 PM"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetails author={faker.person.fullName()} timeAgo="Yesterday at 9:00 PM"/>
+            </ApprovalCard>
+        </div>
     );
 };
 
-
-
 //take the react compoment and show it on the screen
-ReactDom.render(
-    <App />,document.querySelector('#root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
